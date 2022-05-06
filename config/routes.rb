9d8a2698dddb6ec8dd463872c7af4ed4566ee511
Diego Manrique <diego.manrique.m@uni.pe>
2022-05-06 11:37:36 -0500

    Add views devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "tweets#index"
 
   resources :comments
   resources :tweets
-  resources :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
