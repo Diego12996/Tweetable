1a2631f5672aa40ace1d416443cae7373b3c4758
Diego Manrique <diego.manrique.m@uni.pe>
2022-05-06 12:13:03 -0500

    env

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :omniauthable
 
   has_one_attached :avatar
+
+  def self.from_omniauth(auth_hash)
+    where(provider: auth_hash.provider, uid: auth_hash.uid).first_or_create do |user|
+      user.provider = auth_hash.provider
+      user.uid = auth_hash.uid
+      user.username = auth_hash.info.nickname
+      user.name = auth_hash.info.nickname
+      user.email = auth_hash.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
